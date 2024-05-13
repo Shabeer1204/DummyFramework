8c38f4511e66061548e6e0c694644ae00354dba6
Jayaram <Jayaram@jayaram>
2024-05-13 17:07:01 +0530

    first push

---
+++ DummyProject/src/test/java/organizationtest/CreateOrganizationTest.java
@@ -0,0 +1,12 @@
+package organizationtest;
+
+public class CreateOrganizationTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Contact created successfully");
+		
+
+	}
+
+}
